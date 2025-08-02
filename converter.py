7ee897246297b3c3cd4515d3538e2ad1a9aacbab
danigpas <danigpascual@protonmail.com>
2025-08-02 14:00:13 +0200

    terminado el curso de python desde cero!!!!

---
+++ converter.py
@@ -0,0 +1,5 @@
+def celsius_to_farenheit(temp):
+    return (temp * 9/5) + 32
+
+def farenheit_to_celsius(temp):
+    return (temp - 32) * 5/9
---
+++ students.py
@@ -0,0 +1,5 @@
+students = ['pepe','carlo','reixe','dani']
+
+def muesdtra_students(students):
+    for alumno in students:
+        print(alumno)
